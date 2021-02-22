56882747965d18ecc4e9936fd216b79ad68ae254
Kevin Turner <kevinturner@utexas.edu>
2021-02-21 23:16:45 -0600

    Initial Commit

---
+++ Rundometer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Rundometer
+//
+//  Created by Kevin Turner on 2/21/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Rundometer/RundometerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RundometerApp.swift
+//  Rundometer
+//
+//  Created by Kevin Turner on 2/21/21.
+//
+
+import SwiftUI
+
+@main
+struct RundometerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
